5e8cb3468769e527ac5f42b5f15af5971c94ed09
jamesmart77 <jamesmart77@gmail.com>
2019-07-14 20:17:22 -0400

    added notifications and README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Electron For Life
+Very simple app built with [Electron](https://electronjs.org/) to show how simple it is to build cross-platform desktop applications with notifications.
+
+## Setup
+1. `npm install`
+2. `npm run start-watch`
+
--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 const { app, BrowserWindow } = require('electron')
+const notifier = require('node-notifier');
 
 function createWindow () {
-  // Create the browser window.
-  let win = new BrowserWindow({
-    width: 1000,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+    // Create the browser window.
+    let win = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        webPreferences: {
+        nodeIntegration: true
+        }
+    });
+    
+    // Object
+    notifier.notify(
+        {
+            title: 'New Notification',
+            message: 'Please confirm/deny your reservation at desk 03C-582S for today.',
+            timeout: 60, //seconds
+            closeLabel: 'Deny',
+            actions: 'Confirm'
+        }, 
+        function(error, response, metadata) {
+            console.log(response, metadata);
+        });
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
+    // and load the index.html of the app.
+    win.loadFile('index.html')
 }
 
 app.on('ready', createWindow)
